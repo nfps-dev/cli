8e8927a30bc0c96d6fe416d8fe0a0d887e3820b7
Blake Regalia <blake.regalia@gmail.com>
2023-10-04 15:58:05 -0700

    fix: query cmds

--- src/cmds/query.ts
+++ src/cmds/query.ts
@@ -1,5 +1,5 @@
 
-import {cli_entries, cli_exec_contract, cli_query_contract, define_command, exit, load, print, result, type TxOpts} from '../common';
+import {cli_entries, cli_query_contract, define_command} from '../common';
 
 
 export const H_CMDS_QUERY = {
@@ -17,15 +17,7 @@ export const H_CMDS_QUERY = {
 			},
 		},
 		async handler(g_argv) {
-			// query contract
-			const [g_res, xc_code, s_res] = await cli_query_contract(g_argv, g_argv.method!, cli_entries(g_argv.args!));
-
-			// query failed
-			if(xc_code) return exit(s_res);
-
-			// print
-			print('Query response');
-			result(JSON.stringify(g_res, null, '  '));
+			await cli_query_contract(g_argv, g_argv.method!, cli_entries(g_argv.args!));
 		},
 	}),
 };
